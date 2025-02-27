92b5db59010d8156dcd3407928b159af3f9cad00
gyk4j <147011991+gyk4j@users.noreply.github.com>
2025-02-27 11:00:30 +0800

    Add test program and build tool installation

---
+++ build.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+clear
+
+echo '### Initializing build settings...'
+WORKSPACE=$HOME/work/hello/hello
+OUTPUT_DIR=$HOME/work/_temp/build
+echo
+
+echo '### Cleaning output dir...'
+rm -rf $OUTPUT_DIR
+mkdir -p $OUTPUT_DIR
+echo
+
+echo '### Checking out project source...'
+mkdir -p $WORKSPACE
+cp -r /vagrant/* $WORKSPACE
+cd $WORKSPACE
+echo
+
+echo '### Generating build files...'
+cmake \
+  -DCMAKE_CXX_COMPILER=g++ \
+  -DCMAKE_C_COMPILER=gcc \
+  -DCMAKE_BUILD_TYPE=Release \
+  -G "Unix Makefiles" \
+  -DCMAKE_C_FLAGS=-m64 \
+  -DCMAKE_CXX_FLAGS=-m64 \
+  -S $WORKSPACE \
+  -B $OUTPUT_DIR
+echo
+
+#-DCMAKE_EXE_LINKER_FLAGS="-m elf_x86_64" \
+
+echo '### Building...'
+cmake --build $OUTPUT_DIR
+echo
+
+echo '### Checking executable binary'
+file $OUTPUT_DIR/hello
+echo
